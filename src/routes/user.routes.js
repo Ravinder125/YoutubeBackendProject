8ad73faef8d9e75dbc9b6b700c0f3bc0c7dd8ba0
Ravinder <ravinder729090@gmail.com>
2025-02-13 00:16:09 +0530

    add routes for getUserChannelProfile and getWatchHistory; change update methods to PATCH

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
-import { registerUser, loginUser, logoutUser, refreshAccesToken, changePassword, updateAvatar, updateCoverImage } from '../controllers/user.controllers.js';
+import {
+    registerUser, loginUser, logoutUser, refreshAccesToken,
+    changePassword, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory
+} from '../controllers/user.controllers.js';
 import { upload } from '../middlewares/multer.middlewares.js';
 import { verifyJWT } from '../middlewares/auth.middlewares.js';
 
@@ -44,8 +47,9 @@ router.route('/login').post(loginUser);
 router.route('/logout').post(verifyJWT, logoutUser);
 router.route('/refresh-token').post(refreshAccesToken);
 router.route('/change-password').post(verifyJWT, changePassword);
-router.route('/update-avatar').post(verifyJWT, upload.single("avatar"), updateAvatar);
-router.route('/update-coverimage').post(verifyJWT, upload.single("coverImage"), updateCoverImage);
-
+router.route('/update-avatar').patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router.route('/update-coverimage').patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watchHistory").get(verifyJWT, getWatchHistory)
 
 export default router;
